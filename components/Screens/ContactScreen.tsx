3316dae8f4cbf8d54dc766bd96a037305b95c75a
iiebi <bruxell456@gmail.com>
2025-07-07 15:14:47 +0200

    new contact me

--- components/Screens/ContactScreen.tsx
+++ components/Screens/ContactScreen.tsx
@@ -1,29 +1,29 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
 type Props = {
   setScreen: (screen: string) => void;
 };
 
 export default function ContactScreen({ setScreen }: Props) {
   return (
-    <div className="flex flex-col text-[0.2rem] space-y-[0.2rem] text-white">
+    <div className="flex flex-col justify-center text-center text-[0.5rem] space-y-[0.4rem] text-white">
       <span className="text-white " onClick={() => setScreen("home")}>
         Back
       </span>
-
-      <label className="flex flex-col">
-        E-Mail
-        <input
-          type="email"
-          className="bg-black bg-opacity-40 border border-[#007ae8] px-1 py-[1px] text-white"
-        />
-      </label>
-
-      <label className="flex flex-col">
-        Content
-        <input
-          type="text"
-          className="bg-black bg-opacity-40 border border-[#007ae8] px-1 py-[1px] text-white"
-        />
-      </label>
+      <p>Contact</p>
+      <div className="flex flex-row gap-3.5">
+        <Link href="https://www.facebook.com/profile.php?id=61574823005894">
+          <FontAwesomeIcon icon={faFacebook} />
+        </Link>
+        <Link href="https://www.facebook.com/ebrahim.khalil.1">
+          <FontAwesomeIcon icon={faLinkedin} />
+        </Link>
+        <Link href="mailto:hdidaebrahim@gmail.com">
+          <FontAwesomeIcon icon={faEnvelope} />
+        </Link>
+      </div>
     </div>
   );
 }
